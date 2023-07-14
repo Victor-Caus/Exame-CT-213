c8cca164e8147af7504e49894565d7815f1b5805
gubidubi <gubidu0@gmail.com>
2023-07-14 15:40:27 -0300

    Improved spaceship controls

--- ExameCT_Godot/space_challenge/space.gd
+++ ExameCT_Godot/space_challenge/space.gd
@@ -9,16 +9,16 @@ const SELECTED_QUANTITY = 10
 
 var time := 0.0
 
+
 func _ready():
 	generate_first_generation()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	time += delta
-	if time > SELECTION_TIME:
+	while time > SELECTION_TIME:
 		natural_selection()
-		time = 0
+		time -= SELECTION_TIME
 
 
 func generate_first_generation():
@@ -27,7 +27,7 @@ func generate_first_generation():
 		var spaceship = spaceship_resource.instantiate()
 		add_child(spaceship)
 		spaceships.push_back(spaceship)
-		spaceship.mutate()
+		spaceship.nn.mutateNetwork(0.5, 1)
 
 
 func natural_selection():
@@ -41,4 +41,4 @@ func natural_selection():
 		spaceships[i].angular_velocity = Vector3.ZERO
 	for i in range(SELECTED_QUANTITY, spaceships.size()):
 		spaceships[i].nn.layers = spaceships[i%SELECTED_QUANTITY].nn.copyLayers()
-		spaceships[i].mutate()
+		spaceships[i].nn.mutateNetwork(0.05, 0.1)
--- ExameCT_Godot/space_challenge/spaceship.gd
+++ ExameCT_Godot/space_challenge/spaceship.gd
@@ -1,18 +1,13 @@
 extends RigidBody3D
 
-const THRUST = 100.0
+const THRUST = 50.0
 const TURN_ACCELERATION = 10.0
 
 var nn : NN
-var time = 0
 
 
 func _ready():
 	nn = $NN
-
-
-func mutate():
-	nn.mutateNetwork(0.05, 0.1)
 
 
 func _physics_process(_delta):
@@ -20,5 +15,5 @@ func _physics_process(_delta):
 	for i in range(output.size()):
 		output[i] = clamp(output[i], -1, 1)
 	apply_central_force(-basis.z * THRUST * output[0])
-	var torque = Vector3(output[1], output[2], output[3])
+	var torque = quaternion * Vector3(output[1], output[2], output[3]) * TURN_ACCELERATION
 	apply_torque(torque)
