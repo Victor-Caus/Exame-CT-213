d4845cdb52e865e95ebd04e9d1d470ed2f9f4d67
3Disney <victorcauzz@hotmail.com>
2023-07-15 03:01:38 -0300

    Added inertial Weight Schedule
    Now inertia matters less over the time. In thesis, this will make more exploitation and less exploration over the time. This was better? Idk. It keeps converging to a local maximum but the result seems good

--- ExameCT_Godot/space_challenge/Optimization_Algorithms/PSO.gd
+++ ExameCT_Godot/space_challenge/Optimization_Algorithms/PSO.gd
@@ -11,16 +11,18 @@ const SELECTED_QUANTITY = 10
 const INITIAL_MUTATION_CHANCE = 1
 const INITIAL_VARIABILITY = 1
 const MAX_INITIAL_VELOCITY = 2 * INITIAL_VARIABILITY
-const INERTIA_WEIGHT = 0.9
+var INERTIA_WEIGHT : float = 0.9
+const INERTIA_SCHEDULE = 0.01
 const COGNITIVE_P = 0.6
 const SOCIAL_P = 0.8
 
-
+var iteration : int = 0
 var time := 0.0
 
 
 func _ready():
 	generate_first_generation()
+	iteration = 0
 
 
 func _physics_process(delta):
@@ -48,13 +50,22 @@ func natural_selection():
 			# Copy the current NN as the best NN of this particle
 			spaceship.get_child(0).get_child(0).layers = spaceship.get_child(0).copyLayers()
 	
+	# sort so we can get the max
 	spaceships.sort_custom(func(a, b): return a.reward > b.reward)
+	
+	#debug
+	print(iteration)
 	print(spaceships[0].reward)
+	
+	# Reset positions of spaceships
 	for spaceship in spaceships:
 		spaceship.position = Vector3.ZERO
 		spaceship.rotation = Vector3.ZERO
 		spaceship.linear_velocity = Vector3.ZERO
 		spaceship.angular_velocity = Vector3.ZERO
 		spaceship.get_child(0).PSO(INERTIA_WEIGHT, COGNITIVE_P, SOCIAL_P, spaceships[0].get_child(0))
-		
+	
+	# Schedule for inertia weight
+	iteration += 1
+	INERTIA_WEIGHT = INERTIA_WEIGHT / 1 + INERTIA_SCHEDULE * iteration
 
